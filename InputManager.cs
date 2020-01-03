5e920474ad8522674101feb43f6dd934171960ab
coolymike <mikedd2004@gmail.com>
2020-01-03 15:58:22 +0100

    Edited file to update inputs and added SmallRotation

--- InputManager.cs
+++ InputManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 // Token: 0x02000108 RID: 264
 public class InputManager : MonoBehaviour
 {
-	// Token: 0x06000569 RID: 1385 RVA: 0x00006798 File Offset: 0x00004998
+	// Token: 0x06000569 RID: 1385
 	public static bool MenuIsActive(bool _bypassTimeCheck = false)
 	{
 		if (UIMenuManager.AnyMenuActive())
@@ -18,7 +18,7 @@ public class InputManager : MonoBehaviour
 		return InputManager.menuCooldown > 0;
 	}
 
-	// Token: 0x0600056A RID: 1386 RVA: 0x0001AD98 File Offset: 0x00018F98
+	// Token: 0x0600056A RID: 1386
 	public static Vector2 Move(bool allowSlowDown = true)
 	{
 		if (InputManager.MenuIsActive(true))
@@ -33,20 +33,20 @@ public class InputManager : MonoBehaviour
 		return InputManager.move * ((InputManager.LeftShift() && allowSlowDown) ? 0.2f : 1f);
 	}
 
-	// Token: 0x0600056B RID: 1387 RVA: 0x000067C1 File Offset: 0x000049C1
+	// Token: 0x0600056B RID: 1387
 	public static Vector2 MoveLerp(bool allowSlowDown = true)
 	{
 		InputManager.moveLerp = Vector2.Lerp(InputManager.moveLerp, InputManager.Move(allowSlowDown), InputManager.moveAccelerationSpeed * Time.deltaTime);
 		return InputManager.moveLerp;
 	}
 
-	// Token: 0x0600056C RID: 1388 RVA: 0x000067E8 File Offset: 0x000049E8
+	// Token: 0x0600056C RID: 1388
 	public static bool LeftShift()
 	{
 		return !InputManager.MenuIsActive(true) && (Input.GetKey(KeyCode.LeftShift) || InputManager.TouchInput.shift);
 	}
 
-	// Token: 0x0600056D RID: 1389 RVA: 0x0001AE40 File Offset: 0x00019040
+	// Token: 0x0600056D RID: 1389
 	public static Vector2 Look()
 	{
 		if (InputManager.MenuIsActive(false))
@@ -72,7 +72,7 @@ public class InputManager : MonoBehaviour
 		return InputManager.look;
 	}
 
-	// Token: 0x0600056E RID: 1390 RVA: 0x0001AEFC File Offset: 0x000190FC
+	// Token: 0x0600056E RID: 1390
 	public static float VerticalMovement()
 	{
 		float result = 0f;
@@ -91,7 +91,7 @@ public class InputManager : MonoBehaviour
 		return result;
 	}
 
-	// Token: 0x0600056F RID: 1391 RVA: 0x0001AF6C File Offset: 0x0001916C
+	// Token: 0x0600056F RID: 1391
 	public static float HorizontalMovement()
 	{
 		float result = 0f;
@@ -110,179 +110,205 @@ public class InputManager : MonoBehaviour
 		return result;
 	}
 
-	// Token: 0x06000570 RID: 1392 RVA: 0x00006807 File Offset: 0x00004A07
+	// Token: 0x06000570 RID: 1392
 	public static bool Jumping()
 	{
 		return !InputManager.MenuIsActive(false) && (Input.GetKey(GameData.controls.jump) || InputManager.TouchInput.jumping || InputManager.ConsoleInput.XDown());
 	}
 
-	// Token: 0x06000571 RID: 1393 RVA: 0x00006832 File Offset: 0x00004A32
+	// Token: 0x06000571 RID: 1393
 	public static bool Jump()
 	{
 		return !InputManager.MenuIsActive(false) && (Input.GetKeyDown(GameData.controls.jump) || InputManager.TouchInput.jump);
 	}
 
-	// Token: 0x06000572 RID: 1394 RVA: 0x00006856 File Offset: 0x00004A56
+	// Token: 0x06000572 RID: 1394
 	public static bool ToggleRagdollMode()
 	{
 		return (!InputManager.MenuIsActive(false) || LevelManager.StatusIsWinOrLose(true)) && (InputManager.ConsoleInput.TriangleDown() || Input.GetKeyDown(GameData.controls.toggleRagdoll) || InputManager.TouchInput.toggleRagdoll);
 	}
 
-	// Token: 0x06000573 RID: 1395 RVA: 0x00006889 File Offset: 0x00004A89
+	// Token: 0x06000573 RID: 1395
 	public static bool CameraToggle()
 	{
 		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(GameData.controls.switchCamera);
 	}
 
-	// Token: 0x06000574 RID: 1396 RVA: 0x000068A4 File Offset: 0x00004AA4
+	// Token: 0x06000574 RID: 1396
 	public static bool NewCameraAngle()
 	{
 		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(GameData.controls.generateCameraAngle);
 	}
 
-	// Token: 0x06000575 RID: 1397 RVA: 0x000068BF File Offset: 0x00004ABF
+	// Token: 0x06000575 RID: 1397
 	public static bool SetSpawn()
 	{
 		return !InputManager.MenuIsActive(false) && (Input.GetKeyDown(GameData.controls.setSpawn) || InputManager.ConsoleInput.CircleDown() || InputManager.TouchInput.setSpawn);
 	}
 
-	// Token: 0x06000576 RID: 1398 RVA: 0x000068EA File Offset: 0x00004AEA
+	// Token: 0x06000576 RID: 1398
 	public static bool CameraZoom()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L2() || Input.GetKey(GameData.controls.zoom));
 	}
 
-	// Token: 0x06000577 RID: 1399 RVA: 0x0000690E File Offset: 0x00004B0E
+	// Token: 0x06000577 RID: 1399
 	public static bool Grabbing()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R1() || InputManager.PcInput.LeftClick());
 	}
 
-	// Token: 0x06000578 RID: 1400 RVA: 0x00006928 File Offset: 0x00004B28
+	// Token: 0x06000578 RID: 1400
 	public static bool SlowMotionToggle()
 	{
 		return !InputManager.MenuIsActive(false) && (Input.GetKeyDown("joystick button 10") || Input.GetKeyDown(GameData.controls.toggleSlowMotion) || InputManager.TouchInput.toggleSlowMotion);
 	}
 
-	// Token: 0x06000579 RID: 1401 RVA: 0x00006958 File Offset: 0x00004B58
+	// Token: 0x06000579 RID: 1401
 	public static bool ToggleBuildItemMenu()
 	{
 		return Input.GetKeyDown(GameData.controls.toggleBuildItemMenu) || InputManager.TouchInput.toggleBuildItemMenu;
 	}
 
-	// Token: 0x0600057A RID: 1402 RVA: 0x00006972 File Offset: 0x00004B72
+	// Token: 0x0600057A RID: 1402
 	public static bool MountVehicle()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.SquareDown() || InputManager.TouchInput.interact || Input.GetKeyDown(GameData.controls.interact));
 	}
 
-	// Token: 0x0600057B RID: 1403 RVA: 0x0000699D File Offset: 0x00004B9D
+	// Token: 0x0600057B RID: 1403
 	public static bool VehicleA()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R2() || Input.GetKey(GameData.controls.jump) || InputManager.TouchInput.vehicleA || InputManager.PcInput.LeftClick());
 	}
 
-	// Token: 0x0600057C RID: 1404 RVA: 0x000069CF File Offset: 0x00004BCF
+	// Token: 0x0600057C RID: 1404
 	public static bool VehicleB()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L2() || Input.GetKey(KeyCode.LeftShift) || InputManager.TouchInput.vehicleB || InputManager.PcInput.RightClick());
 	}
 
-	// Token: 0x0600057D RID: 1405 RVA: 0x000069FC File Offset: 0x00004BFC
+	// Token: 0x0600057D RID: 1405
 	public static bool Break()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L1() || Input.GetKey(KeyCode.LeftControl));
 	}
 
-	// Token: 0x0600057E RID: 1406 RVA: 0x00006A1B File Offset: 0x00004C1B
+	// Token: 0x0600057E RID: 1406
 	public static bool PlaceObject()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R1() || InputManager.PcInput.LeftClick() || InputManager.TouchInput.place);
 	}
 
-	// Token: 0x0600057F RID: 1407 RVA: 0x00006A3C File Offset: 0x00004C3C
+	// Token: 0x0600057F RID: 1407
 	public static bool RemoveObject()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L1() || InputManager.PcInput.RightClick() || InputManager.TouchInput.remove);
 	}
 
-	// Token: 0x06000580 RID: 1408 RVA: 0x00006A5D File Offset: 0x00004C5D
+	// Token: 0x06000580 RID: 1408
 	public static bool RotateObjectRight()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R2Down() || Input.GetKeyDown(GameData.controls.rotateItemRight) || InputManager.TouchInput.rotateRight);
 	}
 
-	// Token: 0x06000581 RID: 1409 RVA: 0x00006A88 File Offset: 0x00004C88
+	// Token: 0x06000581 RID: 1409
 	public static bool RotateObjectLeft()
 	{
 		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L2Down() || Input.GetKeyDown(GameData.controls.rotateItemLeft) || InputManager.TouchInput.rotateLeft);
 	}
 
-	public static bool RotateObjectXPositive()
-	{
-		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R2Down() || Input.GetKeyDown(KeyCode.Z) || InputManager.TouchInput.rotateRight);
-	}
-
-	// Token: 0x06000581 RID: 1409 RVA: 0x00006A88 File Offset: 0x00004C88
-	public static bool RotateObjectXNegative()
-	{
-		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L2Down() || Input.GetKeyDown(KeyCode.X) || InputManager.TouchInput.rotateLeft);
-	}
-
-		public static bool RotateObjectZPositive()
-	{
-		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.R2Down() || Input.GetKeyDown(KeyCode.C) || InputManager.TouchInput.rotateRight);
-	}
-
-	// Token: 0x06000581 RID: 1409 RVA: 0x00006A88 File Offset: 0x00004C88
-	public static bool RotateObjectZNegative()
-	{
-		return !InputManager.MenuIsActive(false) && (InputManager.ConsoleInput.L2Down() || Input.GetKeyDown(KeyCode.V) || InputManager.TouchInput.rotateLeft);
-	}
-
-	// Token: 0x06000582 RID: 1410 RVA: 0x00006AB3 File Offset: 0x00004CB3
+	// Token: 0x06000582 RID: 1410
 	public static bool ToggleBuildMode()
 	{
 		return InputManager.ConsoleInput.MiddlePadDown() || Input.GetKeyDown(GameData.controls.toggleBuildMode) || InputManager.TouchInput.toggleBuildMode;
 	}
 
-	// Token: 0x06000583 RID: 1411 RVA: 0x00006AD4 File Offset: 0x00004CD4
+	// Token: 0x06000583 RID: 1411
 	public static bool RemoveLastItem()
 	{
 		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(GameData.controls.removeLastItem);
 	}
 
-	// Token: 0x06000584 RID: 1412 RVA: 0x00006AEF File Offset: 0x00004CEF
+	// Token: 0x06000584 RID: 1412
 	public static bool Escape()
 	{
 		return InputManager.ConsoleInput.OptionsDown() || Input.GetKeyDown(KeyCode.Escape) || InputManager.TouchInput.escape;
 	}
 
-	// Token: 0x06000585 RID: 1413 RVA: 0x00006AEF File Offset: 0x00004CEF
+	// Token: 0x06000585 RID: 1413
 	public static bool TogglePauseMenu()
 	{
 		return InputManager.ConsoleInput.OptionsDown() || Input.GetKeyDown(KeyCode.Escape) || InputManager.TouchInput.escape;
 	}
 
-	// Token: 0x06000586 RID: 1414 RVA: 0x00006B08 File Offset: 0x00004D08
+	// Token: 0x06000586 RID: 1414
 	public static bool Back()
 	{
 		return InputManager.ConsoleInput.CircleDown() || Input.GetKeyDown(KeyCode.Escape);
 	}
 
-	// Token: 0x06000587 RID: 1415 RVA: 0x000021BF File Offset: 0x000003BF
+	// Token: 0x06000587 RID: 1415
 	public InputManager()
 	{
 	}
 
-	// Token: 0x06000588 RID: 1416 RVA: 0x00006B1A File Offset: 0x00004D1A
-	// Note: this type is marked as 'beforefieldinit'.
+	// Token: 0x06000588 RID: 1416
 	static InputManager()
 	{
 	}
 
+	// Token: 0x06000768 RID: 1896
+	public static bool RotateObjectXPositive()
+	{
+		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(KeyCode.B);
+	}
+
+	// Token: 0x06000769 RID: 1897
+	public static bool RotateObjectXNegative()
+	{
+		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(KeyCode.N);
+	}
+
+	// Token: 0x0600076A RID: 1898
+	public static bool RotateObjectZPositive()
+	{
+		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(KeyCode.C);
+	}
+
+	// Token: 0x0600076B RID: 1899
+	public static bool RotateObjectZNegative()
+	{
+		return !InputManager.MenuIsActive(false) && Input.GetKeyDown(KeyCode.V);
+	}
+
+	// Token: 0x060007AF RID: 1967
+	public static bool SmallRotation()
+	{
+		if (!InputManager.MenuIsActive(false) && Input.GetKeyDown(KeyCode.I)) {
+			if (SmallRotationActive){
+				SmallRotationActive = false;
+				return false;
+			}
+			else {
+				SmallRotationActive = true;
+				return true;
+			}
+		}
+		else {
+			if (SmallRotationActive){
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
+
 	// Token: 0x040006D4 RID: 1748
 	private static Vector2 move;
+
+	private static bool SmallRotationActive;
 
 	// Token: 0x040006D5 RID: 1749
 	private static Vector2 moveLerp;
@@ -302,19 +328,19 @@ public class InputManager : MonoBehaviour
 	// Token: 0x02000109 RID: 265
 	public static class PcInput
 	{
-		// Token: 0x06000589 RID: 1417 RVA: 0x00006B3A File Offset: 0x00004D3A
+		// Token: 0x06000589 RID: 1417
 		public static bool LeftClick()
 		{
 			return Input.mousePresent && Input.GetKey(KeyCode.Mouse0);
 		}
 
-		// Token: 0x0600058A RID: 1418 RVA: 0x00006B4F File Offset: 0x00004D4F
+		// Token: 0x0600058A RID: 1418
 		public static bool RightClick()
 		{
 			return Input.mousePresent && Input.GetKey(KeyCode.Mouse1);
 		}
 
-		// Token: 0x0600058B RID: 1419 RVA: 0x00006B64 File Offset: 0x00004D64
+		// Token: 0x0600058B RID: 1419
 		public static Vector2 MouseLook()
 		{
 			return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
@@ -324,73 +350,73 @@ public class InputManager : MonoBehaviour
 	// Token: 0x0200010A RID: 266
 	public static class ConsoleInput
 	{
-		// Token: 0x0600058C RID: 1420 RVA: 0x00006B7F File Offset: 0x00004D7F
+		// Token: 0x0600058C RID: 1420
 		public static bool OptionsDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton9);
 		}
 
-		// Token: 0x0600058D RID: 1421 RVA: 0x00006B8B File Offset: 0x00004D8B
+		// Token: 0x0600058D RID: 1421
 		public static bool XDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton1);
 		}
 
-		// Token: 0x0600058E RID: 1422 RVA: 0x00006B97 File Offset: 0x00004D97
+		// Token: 0x0600058E RID: 1422
 		public static bool CircleDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton2);
 		}
 
-		// Token: 0x0600058F RID: 1423 RVA: 0x00006BA3 File Offset: 0x00004DA3
+		// Token: 0x0600058F RID: 1423
 		public static bool SquareDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton0);
 		}
 
-		// Token: 0x06000590 RID: 1424 RVA: 0x00006BAF File Offset: 0x00004DAF
+		// Token: 0x06000590 RID: 1424
 		public static bool TriangleDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton3);
 		}
 
-		// Token: 0x06000591 RID: 1425 RVA: 0x00006BBB File Offset: 0x00004DBB
+		// Token: 0x06000591 RID: 1425
 		public static bool MiddlePadDown()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton13);
 		}
 
-		// Token: 0x06000592 RID: 1426 RVA: 0x00006BC7 File Offset: 0x00004DC7
+		// Token: 0x06000592 RID: 1426
 		public static bool R1()
 		{
 			return Input.GetKey(KeyCode.JoystickButton5);
 		}
 
-		// Token: 0x06000593 RID: 1427 RVA: 0x00006BD3 File Offset: 0x00004DD3
+		// Token: 0x06000593 RID: 1427
 		public static bool R2()
 		{
 			return Input.GetKey(KeyCode.JoystickButton7);
 		}
 
-		// Token: 0x06000594 RID: 1428 RVA: 0x00006BDF File Offset: 0x00004DDF
+		// Token: 0x06000594 RID: 1428
 		public static bool R2Down()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton7);
 		}
 
-		// Token: 0x06000595 RID: 1429 RVA: 0x00006BEB File Offset: 0x00004DEB
+		// Token: 0x06000595 RID: 1429
 		public static bool L1()
 		{
 			return Input.GetKey(KeyCode.JoystickButton4);
 		}
 
-		// Token: 0x06000596 RID: 1430 RVA: 0x00006BF7 File Offset: 0x00004DF7
+		// Token: 0x06000596 RID: 1430
 		public static bool L2()
 		{
 			return Input.GetKey(KeyCode.JoystickButton6);
 		}
 
-		// Token: 0x06000597 RID: 1431 RVA: 0x00006C03 File Offset: 0x00004E03
+		// Token: 0x06000597 RID: 1431
 		public static bool L2Down()
 		{
 			return Input.GetKeyDown(KeyCode.JoystickButton6);
@@ -400,7 +426,7 @@ public class InputManager : MonoBehaviour
 	// Token: 0x0200010B RID: 267
 	public static class TouchInput
 	{
-		// Token: 0x06000598 RID: 1432 RVA: 0x0001AFDC File Offset: 0x000191DC
+		// Token: 0x06000598 RID: 1432
 		public static Vector2 LeftScreen()
 		{
 			if (Input.touchCount == 0)
@@ -427,7 +453,7 @@ public class InputManager : MonoBehaviour
 			return new Vector2(0f, 0f);
 		}
 
-		// Token: 0x06000599 RID: 1433 RVA: 0x0001B148 File Offset: 0x00019348
+		// Token: 0x06000599 RID: 1433
 		public static Vector2 RightScreen()
 		{
 			for (int i = 0; i < Input.touches.Length; i++)
@@ -441,8 +467,7 @@ public class InputManager : MonoBehaviour
 			return new Vector2(0f, 0f);
 		}
 
-		// Token: 0x0600059A RID: 1434 RVA: 0x0001B218 File Offset: 0x00019418
-		// Note: this type is marked as 'beforefieldinit'.
+		// Token: 0x0600059A RID: 1434
 		static TouchInput()
 		{
 		}
