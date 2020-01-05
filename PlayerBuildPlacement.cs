7db4872c68b5964b94cd8fe356601a79f259234e
coolymike <mikedd2004@gmail.com>
2020-01-05 20:16:57 +0100

    Update PlayerBuildPlacement.cs

--- PlayerBuildPlacement.cs
+++ PlayerBuildPlacement.cs
@@ -1,11 +1,11 @@
 using System;
 using UnityEngine;
 
-// Token: 0x020000F0 RID: 240
+// Token: 0x020000F2 RID: 242
 [RequireComponent(typeof(PlayerBuildSystem))]
 public class PlayerBuildPlacement : MonoBehaviour
 {
-	// Token: 0x060004CA RID: 1226
+	// Token: 0x060004D9 RID: 1241 RVA: 0x00005F7F File Offset: 0x0000417F
 	private void Awake()
 	{
 		this.playerSettings = base.transform.root.GetComponent<PlayerSettings>();
@@ -13,16 +13,24 @@ public class PlayerBuildPlacement : MonoBehaviour
 		this.rotation = Quaternion.Euler(0f, 0f, 0f);
 	}
 
-	// Token: 0x060004CB RID: 1227
+	// Token: 0x060004DA RID: 1242 RVA: 0x00005FBD File Offset: 0x000041BD
 	private void Update()
 	{
+		if (InputManager.GridToggle())
+		{
+			PlayerBuildPlacement.gridSize = 0.6f;
+		}
+		else
+		{
+			PlayerBuildPlacement.gridSize = 1E-07f;
+		}
 		if (LevelManager.BuildModeOn)
 		{
 			this.UpdateItemPlacement();
 		}
 	}
 
-	// Token: 0x060004CC RID: 1228
+	// Token: 0x060004DB RID: 1243 RVA: 0x0001A158 File Offset: 0x00018358
 	private void UpdateItemPlacement()
 	{
 		if (this.buildSystem.selectedBuildItemVariant == null)
@@ -63,63 +71,53 @@ public class PlayerBuildPlacement : MonoBehaviour
 		if (InputManager.SmallRotation())
 		{
 			this.rotationAmount = 45f;
-		} else {
-		this.rotationAmount = 1f;
+			return;
 		}
+		this.rotationAmount = 1f;
 	}
 
-	// Token: 0x060004CD RID: 1229
+	// Token: 0x060004DC RID: 1244 RVA: 0x0001A46C File Offset: 0x0001866C
 	public Vector3 PositionToGrid(Vector3 position, float offsetMultiplier = 1f)
 	{
 		return new Vector3(Mathf.Round(position.x / (PlayerBuildPlacement.gridSize * offsetMultiplier)) * PlayerBuildPlacement.gridSize * offsetMultiplier, Mathf.Max(Mathf.Round(position.y / (PlayerBuildPlacement.gridSize * offsetMultiplier)) * PlayerBuildPlacement.gridSize * offsetMultiplier, 0f), Mathf.Round(position.z / (PlayerBuildPlacement.gridSize * offsetMultiplier)) * PlayerBuildPlacement.gridSize * offsetMultiplier);
 	}
 
-	// Token: 0x060004CE RID: 1230
-	public PlayerBuildPlacement()
-	{
-	}
-
-	// Token: 0x060004CF RID: 1231
-	static PlayerBuildPlacement()
-	{
-	}
-
-	// Token: 0x0400064E RID: 1614
+	// Token: 0x0400065C RID: 1628
 	private PlayerSettings playerSettings;
 
-	// Token: 0x0400064F RID: 1615
+	// Token: 0x0400065D RID: 1629
 	private PlayerBuildSystem buildSystem;
 
-	// Token: 0x04000650 RID: 1616
-	public static readonly float gridSize = 0.6f;
+	// Token: 0x0400065E RID: 1630
+	public static float gridSize = 0.6f;
 
-	// Token: 0x04000651 RID: 1617
+	// Token: 0x0400065F RID: 1631
 	public static float boundsOffset = 0.2f;
 
-	// Token: 0x04000652 RID: 1618
+	// Token: 0x04000660 RID: 1632
 	private Vector3 aim;
 
-	// Token: 0x04000653 RID: 1619
+	// Token: 0x04000661 RID: 1633
 	public float distance;
 
-	// Token: 0x04000654 RID: 1620
+	// Token: 0x04000662 RID: 1634
 	public Vector3 position;
 
-	// Token: 0x04000655 RID: 1621
+	// Token: 0x04000663 RID: 1635
 	public Vector3 positionLerp;
 
-	// Token: 0x04000656 RID: 1622
+	// Token: 0x04000664 RID: 1636
 	public Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
 
-	// Token: 0x04000657 RID: 1623
+	// Token: 0x04000665 RID: 1637
 	public Quaternion rotationLerp;
 
-	// Token: 0x04000658 RID: 1624
+	// Token: 0x04000666 RID: 1638
 	private readonly float positionSpeed = 26f;
 
-	// Token: 0x04000659 RID: 1625
+	// Token: 0x04000667 RID: 1639
 	private readonly float rotationSpeed = 16f;
 
-	// Token: 0x040008E6 RID: 2278
+	// Token: 0x04000668 RID: 1640
 	private float rotationAmount = 45f;
 }
